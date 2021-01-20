259f55e0e4b689fd81351d607d33b32be64a18b8
isabel <isl@CO4AEAP000105D8.ap.gbl>
2021-01-20 02:38:29 +0000

    add compose up

--- setup-server.sh
+++ setup-server.sh
@@ -11,4 +11,5 @@ echo $HOSTNAME >> nginx/KeepAliveProbe.aspx
   sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
   sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  sudo docker-compose up --build
 }
