15b1490cb41f4d29c7c97e520a2e0368b9a6a15d
isabel <isl@CO4AEAP000105D8.ap.gbl>
2021-01-20 02:26:45 +0000

    add docker compose error handling

--- setup-server.sh
+++ setup-server.sh
@@ -1,3 +1,14 @@
 cp nginx/KeepAliveProbe_template.aspx nginx/KeepAliveProbe.aspx
 echo $HOSTNAME >> nginx/KeepAliveProbe.aspx
-sudo docker-compose up --build
+
+
+{
+  sudo docker-compose up --build
+} || {
+  sudo rm /usr/local/bin/docker-compose
+  sudo apt remove docker-compose
+  sudo apt autoremove
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+}
